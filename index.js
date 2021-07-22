42c7600438ae278ced7b2e2e2578994ed62a5e27
AKASH  P <84336788+akashp2001@users.noreply.github.com>
2021-07-22 17:21:17 +0530

    uploaded files

---
+++ index.js
@@ -0,0 +1,63 @@
+
+document.querySelector(".circle1").addEventListener("mouseenter", function() {
+  document.querySelector(".innercircle1").style.visibility = "visible";
+  document.querySelector(".innercircle1").classList.add("nik");
+})
+
+document.querySelector(".circle1").addEventListener("mouseleave", function() {
+  document.querySelector(".innercircle1").style.visibility = "hidden";
+})
+
+
+
+document.querySelector(".circle6").addEventListener("mouseenter", function() {
+  document.querySelector(".innercircle6").style.visibility = "visible";
+  document.querySelector(".innercircle6").classList.add("nik");
+})
+
+document.querySelector(".circle6").addEventListener("mouseleave", function() {
+  document.querySelector(".innercircle6").style.visibility = "hidden";
+})
+
+
+
+document.querySelector(".circle2").addEventListener("mouseenter", function() {
+  document.querySelector(".innercircle2").style.visibility = "visible";
+  document.querySelector(".innercircle2").classList.add("nik");
+})
+
+document.querySelector(".circle2").addEventListener("mouseleave", function() {
+  document.querySelector(".innercircle2").style.visibility = "hidden";
+})
+
+
+
+document.querySelector(".circle3").addEventListener("mouseenter", function() {
+  document.querySelector(".innercircle3").style.visibility = "visible";
+  document.querySelector(".innercircle3").classList.add("nik");
+})
+
+document.querySelector(".circle3").addEventListener("mouseleave", function() {
+  document.querySelector(".innercircle3").style.visibility = "hidden";
+})
+
+
+
+document.querySelector(".circle4").addEventListener("mouseenter", function() {
+  document.querySelector(".innercircle4").style.visibility = "visible";
+  document.querySelector(".innercircle4").classList.add("nik");
+})
+
+document.querySelector(".circle4").addEventListener("mouseleave", function() {
+  document.querySelector(".innercircle4").style.visibility = "hidden";
+})
+
+
+document.querySelector(".circle5").addEventListener("mouseenter", function() {
+  document.querySelector(".innercircle5").style.visibility = "visible";
+  document.querySelector(".innercircle5").classList.add("nik");
+})
+
+document.querySelector(".circle5").addEventListener("mouseleave", function() {
+  document.querySelector(".innercircle5").style.visibility = "hidden";
+})
